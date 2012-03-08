0101f845b01697e459e13378d9d1894b41985546
Anni Paakala <anni.paakala@gmail.com>
2012-03-08 23:40:22 +0200

    Some tests and updated users.yml one-admin, two-user

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -5,5 +5,30 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-
+  
+  test "create user with valid parameters" do
+    post :create, user: {email: 'user@mail.com', password: 'password', password_confirmation: 'password'}
+    assert_response :redirect
+    assert_redirected_to root_url
+    assert flash[:notice] 
+  end
+  
+    test "create user with invalid parameters" do
+    post :create, user: {email: 'email'}
+    assert_response :success
+    assert assigns(:user)
+    assert assigns(:user).new_record?
+    assert_template :new
+  end
+  
+  test "destroy user" do
+    user = users(:user)
+    assert_difference("User.count", -1) do
+      delete :destroy, id: user.id
+      assert_response :redirect
+      assert_redirected_to users_path
+      assert flash[:notice]
+    end
+  end
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @user = users(:user)
+  end
+
+  test "a user with all attributes should be valid" do
+    assert @user.valid?
+  end
+  
+  test "a user without a email should not be valid" do
+    @user.email = ''
+    assert @user.invalid?
+  end
+  
+  test "user email must be unique" do
+    other_user = @user.dup
+    assert other_user.invalid?
+  end
+  
 end
